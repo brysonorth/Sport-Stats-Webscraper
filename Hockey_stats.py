a8a45dca5cd32024282e4ffb9cfb4b75ddb62bc8
brysonorth <45219320+brysonorth@users.noreply.github.com>
2020-11-09 17:18:17 -0800

    Add files via upload

--- Hockey_stats.py
+++ Hockey_stats.py
@@ -2,11 +2,29 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 from datetime import datetime
 x = 0
+i = 0
 currentYear = datetime.now().year
+stats = dict()
+listOfStatDic = list()
+
+#Dictionary to convert user input into HTML attribute format
+pointTypeDic = {'games played' : 'games_played', 
+'age' : 'age',
+'goals' : 'goals',
+'assists' : 'assists',
+'points' : 'points',
+'plus/minus' : 'plus_minus',
+'penalty minutes' : 'pen_min',
+'shots' : 'shots',
+'blocks' : 'blocks',
+'hits' : 'hits',
+'faceoff%' : 'faceoff_percentage'}
 print ('\033[1m' + 'NHL Stat Finder' + '\033[0m')
 
 while True: #Loop that ensures stats dictionary is full with stats from season
     #stat dictionary will be empty if stat was not recorded for the given season 
+
+    #block checks if season entry is in range
     urlPart1 = 'https://www.hockey-reference.com/leagues/NHL_'
     urlPart2 = '_skaters.html#stats::points'
     while True: 
@@ -24,64 +42,75 @@ while True: #Loop that ensures stats dictionary is full with stats from season
      
     season = str(season)
     url = urlPart1 + season + urlPart2
-    #url = 'https://www.hockey-reference.com/leagues/NHL_2020_skaters.html#stats::points' #all skater stats 19/20
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
     #putting html into beautiful soup
-
     tags = soup('tr') #finds all 'tr' tags within soup
-    stats = dict()
-    pointTypeDic = {'games played' : 'games_played', 
-    'age' : 'age',
-    'goals' : 'goals',
-    'assists' : 'assists',
-    'points' : 'points',
-    'plus/minus' : 'plus_minus',
-    'penalty minutes' : 'pen_min',
-    'shots' : 'shots',
-    'blocks' : 'blocks',
-    'hits' : 'hits',
-    'faceoff percentage' : 'faceoff_percentage'}
 
-    while True: #Loop to ensure stat is available from scraped stats
-        pointType = input('Which stat do you want to see? ')
+    def statChecker(statListFunc): #function checks if input stats are in pointTypeDic
+        for statType in statListFunc:
+            if statType not in pointTypeDic:
+                print('\n')
+                print(statType, '''is not an available stat. Select a stat from the list
+            Games Played 
+            Age 
+            Goals
+            Assists
+            Points 
+            Plus/Minus
+            Penalty Minutes 
+            Shots 
+            Blocks
+            Hits 
+            Faceoff% ''')
+                return None
+        return statListFunc
+
+    while True: #Loop checks if input stats are available to view in pointTypeDic
+        pointType = input('Which stats do you want to see? ')
         pointType = pointType.lower()
-        if pointType not in pointTypeDic:
-            print('\n')
-            print(pointType, '''is not an available stat. Select a stat from the list
-    Games Played 
-    Age 
-    Goals
-    Assists
-    Points 
-    Plus/Minus
-    Penalty Minutes 
-    Shots 
-    Blocks
-    Hits 
-    Faceoff Percentage ''')
+        statList = pointType.split() #splits pointType inputs into list
+
+        if statChecker(statList) == None:
+             #checks return of statChecker. If no error breaks loop, else continue
             continue
         else:
             break
 
-    for tag in tags: 
-        name = tag.find('a') #finds 'a'attr within 'tr' tag
-        points = tag.find('td', {'data-stat':pointTypeDic[pointType]}) #finds pointTypea within 'td' tag
-        try:
-            points = int(points.string) #if int: convert to int for sorting later
-            stats[name.string] = points #addds to dict
-        except:
-            try: 
-                points = float(points.string) #if float: convert to float for sorting later
+    
+    #function scrapes site and creates a dictionary of player names as keys and pointType as the value
+    def webScraper(stat):     
+        for tag in tags: 
+            name = tag.find('a') #finds 'a'attr within 'tr' tag
+            points = tag.find('td', {'data-stat':stat}) #finds pointTypea within 'td' tag
+            try:
+                points = int(points.string) #if int: convert to int for sorting later
                 stats[name.string] = points #addds to dict
             except:
-                continue
-    if not bool(stats): # if stats is empty - continue loop
-        print(pointType.capitalize(), 'not available for that season.')
-        continue
-    else:
-        break
+                try: 
+                    points = float(points.string) #if float: convert to float for sorting later
+                    stats[name.string] = points #addds to dict
+                except:
+                    continue
+        return stats
 
+    #checks if stat is available for season then appends stat dictionary to list
+    def statAppend(statList): 
+        for stat in statList:
+            if not bool(webScraper(stat)): # if stats is empty returns None and loop continues from start
+                print(pointType.capitalize(), 'not available for', season)
+                return None
+            else:
+                webScraper(stat)
+                statsCopy = stats.copy()
+                listOfStatDic.append(statsCopy) #appends stats dict copy for each stat type. Now have list of dicitonaries
+                return listOfStatDic
+    
+    if statAppend(statList) == None:
+        continue 
+    else: 
+        break
+    
 orderedByPoints = list()
 for k,v in stats.items():
     temp = (v,k)
@@ -99,7 +128,7 @@ while True:
         continue
 
 print('\n')
-print ('\033[1m' + 'Most ' + pointType.capitalize() + '\033[0m') #bolds text 'Most 'pointType'
+print ('\033[1m'  + pointType.capitalize() +  ' Leaders ' + season + '\033[0m') #bolds text 'Most 'pointType'
 
 orderedByPoints = sorted(orderedByPoints, reverse = True)
 for k,v in orderedByPoints[:numberPlayers]:
